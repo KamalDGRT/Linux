9b7e91b1be6a4263146b9f1557bfb35b90c010b4
Kamal Sharma <kamaldgrt@gmail.com>
2020-12-26 15:33:21 +0530

    extract subtiles from mkv files present in a folder

---
+++ Fedora/batchextractsubs.sh
@@ -0,0 +1,13 @@
+mkdir subs;
+for f in *.mkv;
+  do name=`echo "$f"`
+    echo "Extracting subtitle from ${name}"
+    subname="${name%.mkv}.srt"
+    ffmpeg -i "$f" -c:s copy "${subname}" -v quiet -stats
+    mv "${subname}" subs/
+    echo "${subname} has been created in subs/ folder."
+    echo ""
+    echo "-------------------------------------------------------------"
+    echo ""
+done
+
