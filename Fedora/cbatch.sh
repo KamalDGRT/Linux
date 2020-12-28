db20fb14d875b45a1f5217f854292acdbef07dc0
Kamal Sharma <kamaldgrt@gmail.com>
2020-12-28 18:37:43 +0530

    update cbatch.sh

--- Fedora/cbatch.sh
+++ Fedora/cbatch.sh
@@ -1,10 +1,9 @@
 mkdir mux;
 for i in *.mp4;
-  do name=`echo "$i" | cut -d'.' -f1`
-    echo "${name}.mp4"
+  do name=`echo "$i"`
+    echo "${name}"
     echo ""
-    ffmpeg -i "$i" -c:v libx265 -vtag hvc1 "mux/${name}.mp4" -v quiet -stats
-    echo "${name}.mp4 has been converted to h265 fomat."
+    ffmpeg -i "$i" -c:v libx265 -vtag hvc1 "mux/${name}" -v quiet -stats
+    echo "${name} has been converted to h265 fomat."
     echo ""
 done
-
