063bb4dacc38037ad9debd309dd24fb6207ac154
Kamal Sharma <kamaldgrt@gmail.com>
2020-12-28 14:01:06 +0530

    creating mkv files without subtitles

--- Fedora/nosubsmkv.sh
+++ Fedora/nosubsmkv.sh
@@ -1,40 +1,31 @@
-mkdir bmuxmkv;
-echo "Creating bmuxmkv/ to store the output of mkvmerge"
+mkdir nosubsmkv;
+echo "Creating nosubsmkv/ to store the output of mkvmerge"
 echo ""
 
 count=1
-for f in *.mkv;
-  do name=`echo "$f"`
+for file in *.mkv;
+  do name=`echo "$file"`
     outputfile=`echo "${name%.mkv}_1.mkv"`
     echo "${count}"
     echo "Multiplexing file : ${outputfile}"
     inputfile="${name}"
     finalname=`echo "${name%.mkv}.mkv"`
-    subtitlefile="${name%.mkv}.srt"
 
     echo "Input File    : ${inputfile}"
-    echo "Subtitle File : ${subtitlefile}"
     echo "Output File   : ${outputfile}"
-    mkvmerge --ui-language en_US \
- --output "${outputfile}" \
- --language 0:en \
- --language 1:en \
- '(' "${inputfile}" ')'\
- --sub-charset 0:UTF-8\
- --language 0:en\
- '(' "${subtitlefile}" ')'\
- --track-order 0:0,0:1,1:0;
+
+	mkvmerge -o "${outputfile}" --no-subtitles "$inputfile"
 
     echo ""
-    echo "Moving Muxed File to bmuxmkv/"
-    mv "${outputfile}" bmuxmkv/
-    echo "Going inside bmuxmkv/"
-    cd bmuxmkv/
+    echo "Moving Muxed File to nosubsmkv/"
+    mv "${outputfile}" nosubsmkv/
+    echo "Going inside nosubsmkv/"
+    cd nosubsmkv/
     echo "Renaming the file"
     mv "${outputfile}" "${finalname}"
     mkvpropedit "${finalname}" --set title="" --delete-track-statistics-tags --tags all:
     echo "Renamed the file!"
-    echo "Coming out of bmuxmkv/"
+    echo "Coming out of nosubsmkv/"
     cd ..
     echo "-------------------------------------------------------------"
     echo ""
