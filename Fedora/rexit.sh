21333223f238bccd480fddced5fb9f482e0f9f9a
KamalDGRT <kamaldgrt@gmail.com>
2021-06-26 12:54:31 +0530

    modify the rexit script

--- Fedora/rexit.sh
+++ Fedora/rexit.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# References: https://stackoverflow.com/a/60697438
+
+dirName=~/bash_histories
+
+if [ !-d $dirName ];then
+     if ! mkdir $dirName; then
+     echo "Can't make dir: $dirName"
+     fi
+fi
+
 history > ~/"bash_histories/$(date +"%d_%m_%Y_._%I_%M_%S_%p")_bash_history.txt"
 rm ~/.bash_history
 history -c
