57d9c0e522caa19f94d76a835da947a3b7001823
Kamal Sharma <43444282+KamalDGRT@users.noreply.github.com>
2021-08-07 12:56:17 +0530

    removing the part where we run the script as sudo

--- distro/Solus/vanilla_firefox_installer.sh
+++ distro/Solus/vanilla_firefox_installer.sh
@@ -1,27 +1,26 @@
-#!/bin/sh
-if [[ $(id -u) -ne 0 ]] ; then echo "[ERROR] Script needs to be run as root." ; exit 1 ; fi
+#!/bin/bash
+
 {
-printf "\n[INFO] Downloading Firefox\n\n"
-wget https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US
-wait
-printf "\n[INFO] Download Complete\n\n"
-mv firefox-latest-ssl&os=linux64&lang=en-US firefox.tar.bz2
-wait
-tar -xf firefox.tar.bz2
-wait
-mv firefox /opt/
-wait
-{
-	echo "[Desktop Entry]"
-	echo "Encoding=UTF-8"
-	echo "Type=Application"
-	echo "Terminal=False"
-	echo "Exec=/opt/firefox/firefox"
-	echo "Name=Firefox"
-	echo "Icon=/opt/firefox/browser/chrome/icons/default/default128.png"
-} >> /usr/share/applications/firefox.desktop &&
-printf "\n\n[SUCCESS] Firefox installed successfully"
-} || 
-{
-printf "\n\n[ERROR] Could not install firefox. Exiting..."
-}
+    printf "\n[INFO] Downloading Firefox\n\n"
+    wget -O firefox.tar.bz2 'https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US'
+    wait
+    printf "\n[INFO] Download Complete\n\n"
+    wait
+    printf "\n[INFO] Preparing to extract firefox.tar.bz2\n\n"
+    sudo tar -xf firefox.tar.bz2 -C /opt/
+    printf "\n[TASK] Creating a new desktop entry: Firefox (Manual)\n\n"
+    wait
+    {
+        echo "[Desktop Entry]"
+        echo "Encoding=UTF-8"
+        echo "Type=Application"
+        echo "Terminal=False"
+        echo "Exec=/opt/firefox/firefox"
+        echo "Name=Firefox (Manual)"
+        echo "Icon=/opt/firefox/browser/chrome/icons/default/default128.png"
+    } | sudo tee /usr/share/applications/firefox-manual.desktop
+    printf "\n\n[SUCCESS] Firefox installed successfully"
+} ||
+    {
+        printf "\n\n[ERROR] Could not install firefox. Exiting..."
+    }
