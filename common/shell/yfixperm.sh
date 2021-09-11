850b67367bbc4fa9b8f7d0bbd8df8d71514a7055
KamalDGRT <kamaldgrt@gmail.com>
2021-09-12 00:48:09 +0530

    yii2: add 777 to controllers and views - gii

--- common/shell/yfixperm.sh
+++ common/shell/yfixperm.sh
@@ -7,3 +7,7 @@ chmod 777 -R admin/assets/
 chmod 777 -R frontend/runtime
 chmod 777 -R backend/runtime
 chmod 777 -R common/models
+chmod 777 -R backend/controllers/
+chmod 777 -R backend/views/
+chmod 777 -R frontend/controllers/
+chmod 777 -R frontend/views/
